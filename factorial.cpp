5104d470aa87d52bae2ea9ceff65df30b7c783f8
WEB RATTNA <69116458+NantawatTOMOHA@users.noreply.github.com>
2020-09-13 18:09:20 +0700

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+void factorial(long long int, long long int, long long int);
+int main()
+{
+	struct lol {
+		long long int	num[4] = { 0,0,1 };
+};
+	lol f;
+	
+	cout << "===FACTORIAL==="<<endl;
+	cout << "ENTER NUMBER : ";
+	cin >> f.num[0];
+
+	f.num[1] = f.num[0] + 1;
+	factorial(f.num[0],f.num[1],f.num[2]);
+
+
+
+}
+void factorial(long long int k, long long int g, long long int sum)
+{
+	if (k > 0 && k <= 20) {
+		for (long long int i = 0; i < g; i++) {
+			if (k == 0) { break; }
+			if (k == (g - 1)) {
+				sum = sum * k;
+				cout <<"SUMMARY : "<< k;
+				--k;
+
+			}
+			if (k < (g - 1)) {
+				sum = sum * k;
+				cout << " x " << k;
+				--k;
+
+			}
+			
+	
+		}	cout << " = " << sum << endl;
+	}
+	else {
+		cout << " " << endl;
+		cout << "***************" << endl;
+		cout << "**** ERROR ****" << endl;
+		cout << "***************" << endl;
+	}
+
+
+
+}
